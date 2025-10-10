5afa9cb384b89be781f3c7c0e867a9456e849489
Haron <haron.sunde@gmail.com>
2025-10-10 15:45:35 +0200

    Working on adding a main menu to the game with functioning buttons you can click

--- lab5/snake.py
+++ lab5/snake.py
@@ -4,7 +4,7 @@ from game_logic import move_snake
 def app_started(app):
     app.direction = "east"
     app.info_mode = "true"
-    app.state = "active"
+    app.state = "gameover"
     app.timer_delay = 200 # milliseconds
 
     app.board = [
@@ -20,6 +20,14 @@ def app_started(app):
     #slange
     app.snake_size = 3
     app.head_pos = (3,4)
+
+    #GUI
+    app.buttons = [
+        [app.width/2-50, app.height/2-50, app.width/2+50, app.height/2+50, "Start Game", start]
+        ]
+
+def start(app):
+    app.state = "active"
 
 def timer_fired(app):
     if not app.info_mode and app.state == "active":
@@ -45,12 +53,30 @@ def key_pressed(app, event):
         if event.key == "Space" and app.info_mode:
             move_snake(app)
 
+    if app.state == "gameover":
+        if event.key =="r":
+            app_started(app)
+
+#visning
+
 def redraw_all(app, canvas):
     if app.info_mode:
         canvas.create_text(app.width/2, 10, text = f"{app.head_pos=} {app.snake_size=} {app.direction=} {app.state=}", anchor="n")
     
+    if app.state == "gameover":
+        for button in app.buttons:
+            draw_button()
+
     margin = 25
     draw_board(canvas, margin, margin,app.width-margin, app.height-margin, app.board, app.info_mode)
+    
+def draw_button(canvas, button):
+    x1, y1, x2, y2, label, func = button
+    canvas.create_rectangle(x1, y1, x2, y2, fill="lightgray")
+    mid_x = (x1 + x2) / 2
+    mid_y = (y1 + y2) / 2
+    canvas.create_text(mid_x, mid_y, text=label)
+    
         
 if __name__ == '__main__':
     from uib_inf100_graphics.event_app import run_app

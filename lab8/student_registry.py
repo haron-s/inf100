8a2fba3c8be502fadea9ccfc89febb725534cfa1
haron <haron.sunde@gmail.com>
2025-11-04 00:13:19 +0100

    student_registry

--- lab8/student_registry.py
+++ lab8/student_registry.py
@@ -2,14 +2,12 @@ import json
 import csv
 
 
-#def convert_student_to_csv(input, output): 
-with open("students.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
+def convert_student_to_csv(input, output): 
+    with open("students.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        
+    data_list = [
+        [student["id"], student["name"], student["area"], student["year"]]
+        for student in data["students"]
+    ]
 
-data_list = [
-    [id, name, area, year]
-    for person in data["students"]
-    for id, name, area, year in person.get()
-]
-
-print(data_list)
